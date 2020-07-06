07353adb31e5f55241a1e2ba1c35ccf3c28004ac
sxd1226 <67882744+sxd1226@users.noreply.github.com>
2020-07-06 09:17:15 +0800

    Add files via upload

---
+++ test-site/scripts/func.js
@@ -0,0 +1,5 @@
+function mul(num1,num2){
+    let result = num1 * num2;
+    return result;
+}
+
---
+++ test-site/scripts/main.js
@@ -0,0 +1,39 @@
+//let myHeading = document.querySelector('h1');
+//myHeading.textContent = 'Hello World!';
+let myImage = document.querySelector('img');
+myImage.onclick = function(){
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'E:/DS_work/Web/Web-projects/test-site/image/firefox.jpg'){
+        //注意斜杠不能为反斜杠
+        myImage.setAttribute('src','E:/DS_work/Web/Web-projects/test-site/image/nike.jpg');
+    }
+    else{
+        myImage.setAttribute('src','E:/DS_work/Web/Web-projects/test-site/image/firefox.jpg');
+    }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName(){
+    let myName = prompt('请输入你的名字:');//提醒
+    if(!myName || myName === null){
+        setUserName();
+    }
+    else{
+        //将数据存储在家浏览器中,setItem函数创建一个name的数据项,并把myName变量复制给它
+        localStorage.setItem('name',myName);
+        myHeading.textContent = 'Mozilla酷毙了,' + myName;
+    }
+}
+
+if(localStorage.getItem('name')){
+    setUserName();
+}
+else{
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla酷毙了,' + storedName;
+}
+
+myButton.onclick = function(){
+    setUserName();
+}
